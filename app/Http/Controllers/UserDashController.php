26f7e5f5bb98c2e2a89f30e82ed28d599613d5bc
axel <axtev2000@hotmail.com>
2024-11-23 00:09:50 -0500

    dashboard

--- app/Http/Controllers/UserDashController.php
+++ app/Http/Controllers/UserDashController.php
@@ -14,8 +14,7 @@ class UserDashController extends Controller
 
     public function index()
     {
-        
-         return view('dashboard');
-        
+        return view('dashboard');
     }
+
 }
